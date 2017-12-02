d30c11b81dce9e9417c67321d854d33e3c7a5d75
coopeu <consen@consen.org>
2017-12-02 14:34:54 -0500

    social links

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -7,18 +7,20 @@ class ContactsController < ApplicationController
 	def create
     # Instantiate a new object using form parameters
     @contact = Contact.new(params[:contact])
-    @contact.request = request 
-    # Save the object
-    if @contact.deliver
-      # If save succeeds
-      flash.now[:error] = ''
-      flash[:notice] = "Email sent successfully."
-      redirect_to contacts_path
-    else
-      flash.now[:error] = 'Cannot send message.'
-      #render :new
-      redirect_to new_contact_path, :alert => "Fail. Cannot send this message."
-    end
+    @contact.request = request
+    @contact.deliver 
+    redirect_to new_contact_path, notice: "Your message has been sent successfully. We will reply soon."
+#    # Save the object
+#    if @contact.deliver
+#      # If save succeeds
+#      flash.now[:error] = ''
+#      flash[:notice] = "Email sent successfully."
+#      redirect_to contacts_path
+#    else
+#      flash.now[:error] = 'Cannot send message.'
+#      #render :new
+#      redirect_to new_contact_path, :alert => "Fail. Cannot send this message."
+#    end
 	end
 
 end
